c9a7ee7514a27c82f34f0dd6d152d72f06162576
Laila Atrmouh <laila.atrmouh@gmail.com>
2012-09-19 21:54:11 -0400

    User Interface (basically, some switchs, some buttons)

--- UserInterfaceFun/UserInterfaceFun/BIDViewController.h
+++ UserInterfaceFun/UserInterfaceFun/BIDViewController.h
@@ -10,4 +10,39 @@
 
 @interface BIDViewController : UIViewController
 
+// Reference to the name field
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+
+// Reference to the number field
+@property (weak, nonatomic) IBOutlet UITextField *numberTextField;
+
+// Reference to the slider label
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+
+// Handler on the "Do Something" Button
+- (IBAction)buttonPressed:(id)sender;
+
+// Reference to the Do something Button
+@property (weak, nonatomic) IBOutlet UIButton *doSomethingButton;
+
+// Reference to the left switch
+@property (weak, nonatomic) IBOutlet UISwitch *leftSwitch;
+
+// Reference to the right switch
+@property (weak, nonatomic) IBOutlet UISwitch *rightSwitch;
+
+// Handler to close the keyboard
+- (IBAction)textFieldDoneEditing:(id)sender;
+
+// Handler to close the number keyboard
+- (IBAction)backgroundTap:(id)sender;
+
+// Handler triggered when the switchs are changed
+- (IBAction)switchChanged:(id)sender;
+
+// Handler for the segmented controls 
+- (IBAction)toggleControls:(id)sender;
+
+// Handler triggered when the slider has a different value
+- (IBAction)sliderChanged:(id)sender;
 @end
--- UserInterfaceFun/UserInterfaceFun/BIDViewController.m
+++ UserInterfaceFun/UserInterfaceFun/BIDViewController.m
@@ -13,6 +13,12 @@
 @end
 
 @implementation BIDViewController
+@synthesize sliderLabel;
+@synthesize doSomethingButton;
+@synthesize leftSwitch;
+@synthesize rightSwitch;
+@synthesize nameTextField;
+@synthesize numberTextField;
 
 - (void)viewDidLoad
 {
@@ -22,13 +28,57 @@
 
 - (void)viewDidUnload
 {
+    [self setNameTextField:nil];
+    [self setNumberTextField:nil];
+    [self setSliderLabel:nil];
+    [self setLeftSwitch:nil];
+    [self setRightSwitch:nil];
+    [self setDoSomethingButton:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+// Handler to close the keyboard
+- (IBAction)textFieldDoneEditing:(id)sender {
+    [sender resignFirstResponder];
+} 
+
+// Handler to close the number keyboard
+- (IBAction)backgroundTap:(id)sender {
+    [nameTextField resignFirstResponder];
+    [numberTextField resignFirstResponder];
 }
 
+- (IBAction)switchChanged:(id)sender {
+    UISwitch *whichSwitch = (UISwitch *)sender;
+    BOOL setting = whichSwitch.isOn;
+    [leftSwitch setOn:setting animated:YES];
+    [rightSwitch setOn:setting animated:YES];
+}
+
+// Handler on the segmented control
+- (IBAction)toggleControls:(id)sender {
+    // 0 == switches index
+    if ([sender selectedSegmentIndex] == 0) {
+        leftSwitch.hidden = NO;
+        rightSwitch.hidden = NO;
+        doSomethingButton.hidden = YES;
+    }
+    else {
+        leftSwitch.hidden = YES;
+        rightSwitch.hidden = YES;
+        doSomethingButton.hidden = NO;
+    }
+}
+
+// Handler for the change of slider value
+- (IBAction)sliderChanged:(id)sender {
+    UISlider *slider = (UISlider *)sender;
+    int progressAsInt = (int)roundf(slider.value);
+    sliderLabel.text = [NSString stringWithFormat:@"%d", progressAsInt];
+}
+
+// Handler on the "Do Something" Button
+- (IBAction)buttonPressed:(id)sender {
+}
 @end
